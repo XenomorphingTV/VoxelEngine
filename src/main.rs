68f261b7cbace256dd4aedf82f37096b469ace0d
XenomorphingTV <sconovali@hotmail.com>
2024-04-27 21:10:44 +0200

    Fixed the enumeration of devices

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,15 @@
 use vulkano::instance::{Instance, InstanceCreateInfo};
 use vulkano::VulkanLibrary;
 
-fn increment(n: &mut isize) {
-    *n += 1;
-}
-
 fn main() {
     let library = VulkanLibrary::new().expect("no local Vulkan library/DLL");
-    let instance =
-        Instance::new(library, InstanceCreateInfo::default()).expect("failed to create instance");
+    let instance = Instance::new(library, InstanceCreateInfo::default())
+        .expect("failed to create instance");
 
-    let mut i: isize = 0;
-    loop {
-        let physical_device = instance
-            .enumerate_physical_devices()
-            .expect("Could not enumerate physical device")
-            .next()
-            .expect({
-                "No more devices";
-                break;
-            });
-        increment(&mut i);
+    let mut devices_count = 0;
+    for _ in instance.enumerate_physical_devices() {
+        devices_count += 1;
     }
 
-    println!("Devices: {i}");
-
-    let mut length: f32 = 1.0;
-    let mut width: f32 = 1.0;
-    let mut height: f32 = 1.0;
-
-    println!("Hello, world!");
+    println!("Devices: {}", devices_count);
 }
